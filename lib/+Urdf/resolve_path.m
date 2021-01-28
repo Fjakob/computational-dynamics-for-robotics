c11df15a7e87228586ea4a68abdbb9c4e7b1f6d7
nr <nr@inm.uni-stuttgart.de>
2021-01-28 22:01:55 +0100

    update to update to update to proposed fix of #14.

--- lib/+Urdf/resolve_path.m
+++ lib/+Urdf/resolve_path.m
@@ -22,35 +22,9 @@ else
     % get rid of file:\ at beginning of URI
     n = strlength(basePrefix) + 1;
     path = fullfile(dir(n:end), file);
-end
-
-function path = resolve_path(node, file)
-% RESOLVE_PATH Converts a path in a URDF file to a valid OS path.
-%
-%   Note:
-%       All paths are relative to the URDF's DOM base URL (typically the
-%       folder the file is in) unless the FILE is prefixed with package://,
-%       in which case the root directory is ext_lib in the CDR top-level
-%       folder.
-%
-%   See also RESOLVE_MATERIAL
-
-packagePrefix = 'package://';
-basePrefix = 'file:\';
-
-if startsWith(file, packagePrefix)
-    % get rid of package:// at beginning of fmt
-    dir = what('ext_lib').path;
-    n = strlength(packagePrefix) + 1;
-    path = fullfile(dir, file(n:end));
-else
-    dir = fileparts(char(node.getBaseURI()));
-    % get rid of file:\ at beginning of URI
-    n = strlength(basePrefix) + 1;
-    path = fullfile(dir(n:end), file);
     if ismac()
-        path = strrep(filesep, path);
-    end
+        path = fullfile(filesep, path);
+    end    
 end
 
 path = strrep(path, '%20', ' ');
