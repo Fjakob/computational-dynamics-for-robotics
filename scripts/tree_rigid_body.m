f1813f7723749fd659cdd92126f370f161ba5938
Nelson <nr@inm.uni-stuttgart.de>
2021-01-31 22:08:52 +0100

    added minimal file for tree_rigid_body.m

---
+++ scripts/tree_rigid_body.m
@@ -0,0 +1,10 @@
+% TREE_RIGID_BODY A script for demoing the RigidBody class.
+%
+%   See also TREE_URDF_PARSER and RIGIDBODY.
+
+% AUTHORS:
+%   <------------ Add your info! ------------>
+%   Nelson Rosa Jr. nr@inm.uni-stuttgart.de 01/13/2021, Matlab R2020a, v1
+
+
+% Write code to demo your RigidBody class
